7c1a6c251b99d3d7bf43dc594148c37e3f7de655
hbrls <shuaizhexu@gmail.com>
2018-07-10 22:56:56 +0800

    R: egg-web

--- pivot/config/plugin.js
+++ pivot/config/plugin.js
@@ -1,9 +1,12 @@
 'use strict';
 
-// had enabled by egg
-// exports.static = true;
-
 exports.mq = {
   enable: true,
   package: 'egg-mq',
+};
+
+
+exports.web = {
+  enable: true,
+  package: 'egg-web',
 };
